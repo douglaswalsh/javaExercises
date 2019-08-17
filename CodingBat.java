d8fa656a049a600f827223522a0c3077884e8e5e
Douglas Walsh <douglaswalsh3@gmail.com>
2019-08-17 01:12:49 -0400

    more

--- CodingBat.java
+++ CodingBat.java
@@ -3,7 +3,7 @@ package codingBatPractice;
 public class CodingBat {
 
 	public static void main(String[] args) {
-		ArmstrongNumbers a = new ArmstrongNumbers();
-		System.out.println(a.isArmstrongNumber(154));
+		RnaTranscription a = new RnaTranscription();
+				System.out.println(a.transcribe("ACGTGGTCTTAA"));
 	}
 }
--- RnaTranscription.java
+++ RnaTranscription.java
@@ -3,8 +3,27 @@ package codingBatPractice;
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-       return "hi";
+     String rna = "";
+    	for (int i = 0;i < dnaStrand.length();i++){
+    	  char c = dnaStrand.charAt(i);
+    	  if (c == 'G'){
+    		 rna += "C"; 
+    	  }
+    	  if (c == 'C'){
+     		 rna += "G"; 
+     	  }
+    	  if (c == 'T'){
+    		  rna += "A";
+    	  }
+    	  if (c == 'A'){
+    		  rna += "U";
+    	  }
+      }
+    	
+    	return rna;
     }
 
 }
+
+
 
