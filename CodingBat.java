df444523ea9ee706bc56be96dbf3695973fcf174
Douglas Walsh <douglaswalsh3@gmail.com>
2019-08-17 21:12:19 -0400

    dnd character working

--- CodingBat.java
+++ CodingBat.java
@@ -4,11 +4,12 @@ public class CodingBat {
 
 	public static void main(String[] args) {
 		DnDCharacter d = new DnDCharacter();	
-		System.out.println(Math.floor(-3.5));
-		System.out.println("Strength = \t" + d.getStrength());
-		System.out.println("charisma = \t" + d.getCharisma());
-		System.out.println("Wisdom = \t" + d.getWisdom());
-		System.out.println("Constitution = \t" + d.getConstitution());
-		System.out.println(d.modifier(3));
+		System.out.println(d.getHitpoints());
+		System.out.println(d.getConstitution());
+		System.out.println(10 + d.modifier(d.getConstitution()));
 	}
 }
+/*
+ * assertEquals(character.getHitpoints(),10 +
+ * character.modifier(character.getConstitution()));
+ */
--- DnDCharacter.java
+++ DnDCharacter.java
@@ -4,72 +4,100 @@ import java.util.Arrays;
 import java.util.Random;
 
 class DnDCharacter {
-	
-	//this does not calculate each value only once - how to do that?
-	
+
 	private int strength;
+	boolean firstRunStrength = true;
 	private int dexterity;
+	boolean firstRunDexterity = true;
 	private int constitution;
+	boolean firstRunConstitution = true;
 	private int intelligence;
+	boolean firstRunIntelligence = true;
 	private int charisma;
+	boolean firstRunCharisma = true;
 	private int wisdom;
-		
+	boolean firstRunWisdom = true;
+
 	int ability() {
 		int ability;
 		int[] results = new int[4];
 		Random rand = new Random();
-		for (int i = 0; i < results.length;i++){
-			int roll = rand.nextInt(6)+1;
+		for (int i = 0; i < results.length; i++) {
+			int roll = rand.nextInt(6) + 1;
 			results[i] = roll;
 		}
 		Arrays.sort(results);
 		return ability = results[1] + results[2] + results[3];
-    }
+	}
 
-    int modifier(int input) {
-     //return  (int) (input - 10 > 0 ? (input - 10)/2 : Math.floor((input - 10)/2));
-    	if (input - 10 > 0){
-    		return (input - 10)/2;
-    	} else{
-    		System.out.println("did the else");
-    		int i = input - 10;
-    		Double d = new Double(i);
-    		d = d/2;
-    		System.out.println("d " + d);
-    		d = Math.floor(d);
-    		System.out.println("double = " + d);
-    		 int b = d.intValue();
-    		return b;
-    	}
-    }
+	int modifier(int input) {
+		// return (int) (input - 10 > 0 ? (input - 10)/2 : Math.floor((input -
+		// 10)/2));
+		if (input - 10 > 0) {
+			return (input - 10) / 2;
+		} else {
+			// System.out.println("did the else");
+			int i = input - 10;
+			Double d = new Double(i);
+			d = d / 2;
+			// System.out.println("d " + d);
+			d = Math.floor(d);
+			// System.out.println("double = " + d);
+			int b = d.intValue();
+			return b;
+		}
+	}
 
-    final int getStrength() {
-      return strength = ability();
-    }
+	 int getStrength() {
+		if (firstRunStrength) {
+			firstRunStrength = false;
+			return strength = ability();
+		}
+		return strength;
+	 }
 
-   final int getDexterity() {
-    	return dexterity = ability();
-    }
+	 int getDexterity() {
+		if (firstRunDexterity) {
+			firstRunDexterity = false;
+			return dexterity = ability();
+		}
+		return dexterity;
+	}
 
-   final int getConstitution() {
-    	return constitution = ability();
-    }
+	 int getConstitution() {
+		if (firstRunConstitution) {
+			firstRunConstitution = false;
+			return constitution = ability();
+		}
+		return constitution;
+	}
 
-  final  int getIntelligence() {
-    	return intelligence = ability();
-    }
+	 int getIntelligence() {
+		if (firstRunIntelligence) {
+			firstRunIntelligence = false;
+			return intelligence = ability();
+		}
+		return intelligence;
+	}
 
-   final int getWisdom() {
-    	return wisdom = ability();
-    }
+	 int getWisdom() {
+		if (firstRunWisdom) {
+			firstRunWisdom = false;
+			return wisdom = ability();
+		}
+		return wisdom;
+	}
 
-   final int getCharisma() {
-    	return charisma = ability();
-    }
+	 int getCharisma() {
+		if (firstRunCharisma) {
+			firstRunCharisma = false;
+			return charisma = ability();
+		}
+		return charisma;
+	}
 
-    int getHitpoints() {
-       return 10 + modifier(constitution);
-    }
+	int getHitpoints() {
+		return 10 + modifier(getConstitution());
+	}
 
 }
-
