8696e28a96707234bb9dcdbe4eeaf5fecdcbd9bb
Douglas Walsh <douglaswalsh3@gmail.com>
2019-08-20 22:59:22 -0400

    pangram workin

--- CodingBat.java
+++ CodingBat.java
@@ -4,7 +4,8 @@ public class CodingBat {
 
 	public static void main(String[] args) {
 		PangramChecker p = new PangramChecker();
-		System.out.println(p.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
+		System.out.println(p.isPangram("\"Five quacking Zephyrs jolt my wax bed.\""));
+		System.out.println("\"Five quacking Zephyrs jolt my wax bed.\"");
 	}
 }
 
--- PangramChecker.java
+++ PangramChecker.java
@@ -10,12 +10,13 @@ public class PangramChecker {
     public boolean isPangram(String input) {
     	//check if input matches alphabet, doesn't care about spaces, order, cases
     	//convert string to char array
-    	char[] tempArray = input.toCharArray();
+    	String lowerString = input.toLowerCase().replaceAll("[^a-z]", "");
+    	
+    	char[] tempArray = lowerString.toCharArray();
     	//sort char array in alphabetical order
     	Arrays.sort(tempArray);
     	//remove duplicates from the char array and add resulting chars to the string
     	boolean repeatedChar;
-    	//String sortedInput = new String(tempArray);
     	String newString = "";
     	for (int i = 0; i <tempArray.length;i++){
     		repeatedChar = false;
@@ -27,11 +28,9 @@ public class PangramChecker {
     		}
     		if (!repeatedChar){
     			newString = newString + tempArray[i];
-    			//System.out.println("getting in there");
     		}
     	}
-    	//removes spaces from the string
-    	newString = newString.replaceAll("[^a-zA-Z0-9]", "");
+    	System.out.println("new string before " + newString);
     	System.out.println("this is the newString \t"+ newString);
     	System.out.println("this is the pattern \t"+ pattern);
     	return Pattern.matches(pattern, newString);
