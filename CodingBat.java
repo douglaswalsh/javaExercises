6e70af1dc54501f36e05d2fcc1237ffd8fa4b03a
Douglas Walsh <douglaswalsh3@gmail.com>
2019-08-21 16:38:28 -0400

    hamming exercise

--- CodingBat.java
+++ CodingBat.java
@@ -3,9 +3,8 @@ package codingBatPractice;
 public class CodingBat {
 
 	public static void main(String[] args) {
-		PangramChecker p = new PangramChecker();
-		System.out.println(p.isPangram("the quick brown fox jumps over the lazy dg"));
-		System.out.println(p.Counter.size());
+		Hamming h = new Hamming("A","Ba");
+		System.out.println(h.getHammingDistance());
 	}
 }
 
